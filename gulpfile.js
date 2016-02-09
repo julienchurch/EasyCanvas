69e25a41fe696eec312b25e389157cb8e8f32e8b
Julien Church <julienchurch@gmail.com>
2016-02-09 15:33:45 -0500

    Add vendor code compilation to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp    = require('gulp'),
-    babel   = require('gulp-babel'),
     jshint  = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     concat  = require('gulp-concat'),
@@ -12,17 +11,28 @@ gulp.task('js-hint', function() {
       .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('js-compile', function() {
+gulp.task('js-compile_src', function() {
   gulp.src('src/*.js')
       .pipe(concat('main.js'))
       .pipe(uglify())
       .pipe(gulp.dest('./build'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.js', ['js-hint']);
+gulp.task('js-compile_vendor', function() {
+  gulp.src('vendor/*.js')
+      .pipe(concat('vendor.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./build'))
 });
 
-gulp.task('default', [ 'js-hint'
-                     , 'watch' ]);
+gulp.task('watch', function() {
+gulp.watch('src/*.js', ['js-hint'
+                       ,'js-compile_src'
+                       ,'js-compile_vendor']);
+});
+
+gulp.task('default', ['js-hint'
+                     ,'js-compile_src'
+                     ,'js-compile_vendor'
+                     ,'watch']);
   
