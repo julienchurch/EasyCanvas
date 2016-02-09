72b54b9d310d363eaeb7c6241a55975a93849da7
Julien Church <julienchurch@gmail.com>
2016-02-09 04:08:34 -0500

    Add basic functionality
    Added concat, uglify, linting, renaming, and outputting functionality.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp    = require('gulp'),
+    babel   = require('gulp-babel'),
+    jshint  = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    concat  = require('gulp-concat'),
+    rename  = require('gulp-rename'),
+    uglify  = require('gulp-uglify');
+
+gulp.task('js-hint', function() {
+  gulp.src('src/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('js-compile', function() {
+  gulp.src('src/*.js')
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./build'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.js', ['js-hint', 'js-compile']);
+});
+
+gulp.task('default', [ 'js-hint'
+                     , 'js-compile'
+                     , 'watch' ]);
+  
