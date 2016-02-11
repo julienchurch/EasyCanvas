ecde24f13f6e0a767513df1d2479b667f507f5bf
Julien Church <julienchurch@gmail.com>
2016-02-11 07:04:20 -0500

    Remove cruft

--- examples/example.js
+++ examples/example.js
@@ -1,28 +1,3 @@
-function lblur(img) {
-  for (var i=0; i<30; i++) {
-    var lightblur = new LightFX();
-    lightblur.resizeTo(img);
-    lightblur.drawCover(img);
-    var qbb = lightblur.lightblur(40);
-    document.body.appendChild(qbb);
-  }
-}
-
-function sblur(img) {
-  for (var i=0; i<30; i++) {
-    var stackblur = new LightFX();
-    stackblur.resizeTo(img);
-    stackblur.drawCover(img);
-    stackblur.blur("stackblur", 40);
-    document.body.appendChild(stackblur.canvas);
-  }
-}
-
-function binmoid(t) {
-  return 255 / (1 + Math.pow(Math.E, -t));
-} 
-
-
 window.addEventListener("load", function() {
   var img = document.getElementById("image");
   var lblur = new LightFX(img);
